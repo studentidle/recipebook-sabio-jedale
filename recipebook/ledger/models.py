27dae43ccee9f8447667fe9bbba44f47c0c00930
JavaDucks <jedale.sabio@student.ateneo.edu>
2025-03-07 23:53:04 +0800

    added code optimization

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,18 +1,27 @@
 from django.db import models
 
 class Ingredient(models.Model):
+    '''
+        accepts ingredient name
+    '''
     name = models.CharField(max_length=255)
     
     def __str__(self):
         return f'{self.name}'
 
 class Recipe(models.Model):
+    '''
+        accepts receipe name
+    '''
     name = models.CharField(max_length=255)
     
     def __str__(self):
         return f'{self.name}'
 
 class RecipeIngredient(models.Model):
+    '''
+        accepts ingredient, recipe and quantity
+    '''
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
     quantity = models.IntegerField()
