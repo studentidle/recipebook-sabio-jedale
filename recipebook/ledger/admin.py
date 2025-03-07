0c7c63234d6bcf99d03bf6a90f91aab1d1508935
JavaDucks <jedale.sabio@student.ateneo.edu>
2025-03-07 23:53:04 +0800

    added code optimization

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -2,20 +2,26 @@ from django.contrib import admin
 from .models import Ingredient, Recipe, RecipeIngredient
 
 class IngredientAdmin(admin.ModelAdmin):
-    
+    '''
+        show name, have search fields and filter
+    '''
     model = Ingredient
     list_display = ('name',)
     search_fields = ('name',)
     list_filter = ('name',)
 
 class RecipeAdmin(admin.ModelAdmin):
-    
+    '''
+        show name, have search fields 
+    '''
     model = Recipe
     list_display = ('name', )
     search_fields = ('name', )
 
 class RecipeIngredientAdmin(admin.ModelAdmin):
-
+    '''
+        display receipe, ingredient and quantity also have filter
+    '''
     model = RecipeIngredient
     list_display = ('recipe', 'ingredient', 'quantity', )
     list_filter = ('recipe', 'ingredient', )
--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -2,21 +2,23 @@ from django.shortcuts import render, HttpResponse
 from .models import Recipe, RecipeIngredient
 
 def recipeList(request):
+    '''
+        for listing recipes
+    '''
     recipe = Recipe.objects.all()
-    
     ctx = {
         'recipes': recipe
     }
-    
     return render(request, "recipeList.html", ctx)
 
 def recipe(request, id):
+    '''
+        for detailed view of the specific recipe and its ingredients
+    '''
     recipe = Recipe.objects.get(id=id)
     recipeIngredients = RecipeIngredient.objects.filter(recipe=recipe)
-    
     ctx = {
         'recipe_ingredients': recipeIngredients,
         'recipe': recipe,
     }
-    
     return render(request, "recipe.html", ctx)
