e92792268a063d397ab52156792e7f2151101df6
studentidle <jedale.sabio@student.ateneo.edu>
2025-04-07 22:44:40 +0800

    added RecipeForm

--- recipebook/ledger/forms.py
+++ recipebook/ledger/forms.py
@@ -1,2 +1,7 @@
 from django import forms
 from .models import Ingredient, Recipe, RecipeIngredient, RecipeImage
+
+class RecipeForm(forms.ModelForm):
+    class Meta:
+        model = Recipe
+        fields = ['name']
