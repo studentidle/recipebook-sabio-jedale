85ad25ba52087692846d6d2b86a73a33131db797
studentidle <jedale.sabio@student.ateneo.edu>
2025-04-07 22:13:57 +0800

    added models Image, Ingredient and Profile in views

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Recipe, RecipeIngredient
+from .models import Recipe, RecipeIngredient, RecipeImage, Ingredient, Profile
 from django.contrib.auth.decorators import login_required
 
 def home(request):
