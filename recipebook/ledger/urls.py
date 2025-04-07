b4e40092e9f3747bf40be1940b0c630e7aa9d874
studentidle <jedale.sabio@student.ateneo.edu>
2025-04-07 23:31:56 +0800

    added url paths

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -5,6 +5,9 @@ urlpatterns = [
     path('recipe/list', views.recipeList, name='recipe-list'),
     path('recipe/<int:id>', views.recipe, name='recipe'),
     path('home', views.home, name='home'),
+    path('recipe/add/', views.addRecipe, name='addRecipe'),
+    path('recipe/add/ingredient', views.addIngredient, name='addIngredient'),
+    path('recipe/<int:num>/add_image', views.addImage, name='addImage'),
 ]
 
 app_name = "ledger"
