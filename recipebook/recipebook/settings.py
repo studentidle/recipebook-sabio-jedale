c06c1bc28efcafe0b822185e3bef285bc3d3648c
studentidle <jedale.sabio@student.ateneo.edu>
2025-04-08 01:36:30 +0800

    fixed bugs in adding hidden env key

--- recipebook/recipebook/settings.py
+++ recipebook/recipebook/settings.py
@@ -119,10 +119,10 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_URL = '/static/'
 LOGIN_URL = '/home'
 LOGIN_REDIRECT_URL = '/recipe/list'
 LOGOUT_REDIRECT_URL = '/home'
