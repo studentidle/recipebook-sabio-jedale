206ceece51ec6f8ffbf726aeb8f52b997b11b9d5
studentidle <jedale.sabio@student.ateneo.edu>
2025-04-07 23:27:00 +0800

    added addRecipe Form in views

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Recipe, RecipeIngredient, RecipeImage, Profile
 from .forms import RecipeForm, RecipeIngredientForm, IngredientForm, RecipeImageForm
 from django.contrib.auth.decorators import login_required
@@ -34,5 +34,46 @@ def recipe(request, id):
         'images': images,
     }
     return render(request, "recipe.html", context)
+
+@login_required
+def addRecipe(request):
+
+    if request.method == 'POST':
+
+        recipeForm = RecipeForm(request.POST)
+        recipeIngredientForm = RecipeIngredientForm(request.POST)
+        
+        if recipeForm.is_valid(): 
+                recipe = recipeForm.save(commit=False)
+                profile = Profile.objects.get(user = request.user)
+                recipeName = recipeForm.cleaned_data.get('name')
+
+                if Recipe.objects.filter(name__iexact=recipeName, author=profile).exists():
+                    recipeForm.add_error(None, 'Recipe already exists.')
+                else:
+                    recipe.name = recipeName
+                    recipe.author = profile
+                    recipe.save()
+                    return redirect('ledger:addRecipe')
+            
+        if recipeIngredientForm.is_valid():
+
+                recipeIngredient = recipeIngredientForm.save(commit=False)
+                recipe = recipeIngredient.recipe
+
+                recipeIngredient.save()
+                recipe.save()
+                return redirect('ledger:recipeList')
+
+    else:
+        recipeForm = RecipeForm()
+        recipeIngredientForm = RecipeIngredientForm()
+
+    context = {
+        'recipe': recipeForm, 
+        'recipeIngredient': recipeIngredientForm
+    }
+    
+    return render(request, 'addRecipe.html', context)
 
 
